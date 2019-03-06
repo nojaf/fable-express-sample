9744f9edb8280c40fb41d690b07e0da80f2cdf1c
Verdonck <SIDFLOV@sidmar.be>
2019-03-06 14:43:10 +0100

    init version

---
+++ src/Library.fs
@@ -0,0 +1,13 @@
+﻿module App
+
+open Fable.Core.JsInterop
+open Fable.Import
+
+let app = express.Invoke()
+let port = 3000
+
+app.get (!^ "/", (fun _ (res: express.Response) _ -> res.send("works") |> box)) |> ignore
+
+app.listen(port, fun _ -> 
+    printfn "server started on %i" port
+) |> ignore
